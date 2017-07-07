f8d82ede2791fdd130ac7bc86f5eb06a74f5c478
wzz <2573775798@qq.com>
2017-07-07 17:11:58 +0800

    wzz

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+     <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+    @import "/static/css/base.css";
+</style>
